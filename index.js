633f3b07323181283f48bfe402c3216db5dd4e59
anamul101 <rjanamul01871@gmail.com>
2022-11-04 19:55:14 +0000

    ubdate

--- index.js
+++ index.js
@@ -13,10 +13,27 @@ app.use(express.json());
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.apqupzl.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run(){
+    try{
+        const productsCollection = client.db('emajhondb').collection('products');
+        app.get('/products', async(req,res)=>{
+            const page = req.query.page;
+            const size = req.query.size;
+            console.log(page,size)
+            const query = {};
+            const cursor= productsCollection.find(query);
+            const products = await cursor.toArray();
+            const count = await productsCollection.estimatedDocumentCount(cursor)
+            res.send({count ,products})
+        })
+    }
+    finally{
 
+    }
+}
+run().catch(error=>console.error(error))
 
 
 app.get('/', (req, res) => {
